1a18dbfd69a93f3b258efe69059e1fb230060324
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-01-03 13:50:57 +0200

    initial commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("From the frontend");
 
 const alldishes = document.querySelector("#all_dishes");
 
-const url = 'http://localhost:3000/dishes/all';
+const url = 'https://nodejs-middle-project.onrender.com/dishes/all';
 
 
 let dishes;
@@ -42,7 +42,7 @@ function printDishes(dishes){
 
 async function fetchUserInfo() {
     try {
-      const response = await fetch('http://localhost:3000/clients/all');
+      const response = await fetch('https://nodejs-middle-project.onrender.com/clients/all');
       const data = await response.json();
       const clients = data.clients;
       console.log(clients)
